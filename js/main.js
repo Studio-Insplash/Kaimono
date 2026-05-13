a996d9f0748d2067ca31ce8cfa366b003b526cd2
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-05-13 22:15:56 +0900

    feat: add item registration and checkbox generation for shopping list (WIP)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // TODO: input -> register -> generate shopping item with checkboxの実装
+let itemInput = document.getElementById("itemInput");
+let addButton = document.getElementById("addButton");
+let shoppingList = document.getElementById("shoppingList");
+
+addButton.addEventListener("click", () => {
+    // shoppingList変数をベースにli, input-checkbox, labelを使いitemInputをTODOリストとして登録する。
+})
