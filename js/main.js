cab43e14820e8d4ba70c7082d9b77d04effa39d6
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-05-15 14:04:49 +0900

    feat: add functionality to generate shopping list items as checkboxes from user input

--- js/main.js
+++ js/main.js
@@ -5,4 +5,26 @@ let shoppingList = document.getElementById("shoppingList");
 
 addButton.addEventListener("click", () => {
     // shoppingList変数をベースにli, input-checkbox, labelを使いitemInputをTODOリストとして登録する。
+
+    // idを事前に用意
+    let id = "item-" + Date.now();
+
+    // li要素を作成
+    let li = document.createElement("li");
+    li.className = "shoppingItem";
+
+    // checkbox作成
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = id;
+
+    // label作成
+    let label = document.createElement("label");
+    label.textContent = itemInput.value;
+    label.htmlFor = checkbox.id;
+
+    // つなぐ
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    shoppingList.appendChild(li);
 })
