a615f5d4362a3858653d6836c2284e9cf1b8bff5
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-05-27 19:07:21 +0900

    feat: make service worker apply immediately

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', (event) => {
     console.log('Service Worker installed');
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
 });
